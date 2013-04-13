861cb3ed235709a82549e2af54c6a6fa1c0e7022
shaoqiang <scu.2007@yahoo.com.cn>
2013-04-13 17:08:05 +0800

    add a new java class

---
+++ src/com/example/android_app1/Picture.java
@@ -0,0 +1,16 @@
+package com.example.android_app1;
+
+public class Picture {
+	int height = 0;
+	int weight = 0;
+	
+	int getHeight() {
+		return height;
+	}
+	
+	int getWeight() {
+		return weight;
+	}
+}
+
+
