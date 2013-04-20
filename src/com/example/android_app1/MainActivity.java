ba7b0fd117dba9ccb0d7048e9d4eb1dffafa07e1
shaoqiang <scu.2007@yahoo.com.cn>
2013-04-20 13:29:06 +0800

    add webview

--- src/com/example/android_app1/MainActivity.java
+++ src/com/example/android_app1/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.android_app1;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.webkit.WebView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+    WebView browser;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.main_layout);
+    	browser = (WebView)findViewById(R.id.webkit);
+    	browser.loadUrl("http://huaban.com/favorite/beauty/");
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
